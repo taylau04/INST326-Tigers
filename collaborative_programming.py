c2d8278eae60ee7c274b027fa3c430b0907c3911
Aadarsh Bandyopadhyay <aadarshbandyopadhyay@gmail.com>
2023-11-09 19:27:52 -0500

    First version of performance graph

--- collaborative_programming.py
+++ collaborative_programming.py
@@ -8,7 +8,43 @@
 
 """ This is my docstring! - Aadarsh """
  
- """ rene made this - rene """
-"test"
+""" rene made this - rene """
 
-hahahaha 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+class CollegeBasketballPlayer:
+    
+    def __init__(self, name):
+        self.scores = []
+        self.name = name
+        
+    def add_score(self, score):
+        self.scores.append(score)
+    
+    def performance_graph(self):
+
+        df = pd.DataFrame({'Score': self.scores, 'Game': range(1, len(self.scores) + 1)})
+
+        # Calculate the average score.
+        average_score = df['Score'].mean()
+
+        # Plot the bar using seaborn
+        sns.barplot(x='Game', y='Score', data=df)
+
+        # Add a horizontal line for the average score using Matplotlib.
+        plt.axhline(average_score, color='black', linestyle='dashed', linewidth=2)
+
+        # Annotate the average line.
+        plt.text(len(self.scores)-1, average_score, f'Average: {average_score:.1f}',
+                color='red', va='top', ha='right')
+
+        # Adding labels and title
+        plt.xlabel(f'Games')
+        plt.ylabel('Score')
+        plt.title(f'Scores of {self.name} for multiple games')
+
+        # Display the plot.
+        plt.show()
+
